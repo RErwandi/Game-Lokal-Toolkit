cd5b895080fdf30013797c107bd584d591fd13e2
Reynold Erwandi <reynolderwandi@gmail.com>
2020-05-12 23:59:51 +0700

    Add Singleton and Logger Utility

---
+++ Runtime/Common/Logger/Log.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GameLokal
+{
+    public static class Log
+    {
+        private static bool SHOW_DEBUG = true;
+        
+        public static void Show(string content, string type , params object[] parameters)
+        {
+            if (!SHOW_DEBUG) return;
+            Debug.LogFormat($"<color=#FF3333>[GL_{type}]</color> {content}", parameters);
+        }
+    }
+}
---
+++ Runtime/Utility/Singleton/Singleton.cs
@@ -0,0 +1,71 @@
+﻿namespace GameLokal.Utility
+{
+	using UnityEngine;
+
+	public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+	{
+		private const string MSG_MULTI_INSTANCE = "Multiple instances of a singleton '{0}' exist";
+		private const string MSG_CREATE_INSTANCE = "Creating a singleton instance of '{0}'";
+		private const string MSG_EXIST_INSTANCE = "Using '{0}' instance";
+		private const string DEBUG_TYPE = "Singleton";
+
+		private static T _instance;
+
+		// CONSTRUCTOR: ---------------------------------------------------------------------------
+
+		public static T Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = (T) FindObjectOfType(typeof(T));
+
+					if (FindObjectsOfType(typeof(T)).Length > 1)
+					{
+						Log.Show(MSG_MULTI_INSTANCE, DEBUG_TYPE, _instance.gameObject.name);
+
+						return _instance;
+					}
+
+					if (_instance == null)
+					{
+						CreateInstance();
+					} 
+					else 
+					{
+						Log.Show(MSG_EXIST_INSTANCE, DEBUG_TYPE, _instance.gameObject.name);
+					}
+				}
+
+				return _instance;
+			}
+		}
+
+		// VIRTUAL METHODS: -----------------------------------------------------------------------
+
+		protected virtual void OnCreate() { }
+
+		protected virtual bool ShouldNotDestroyOnLoad() { return true; }
+
+		// PRIVATE METHODS: -----------------------------------------------------------------------
+
+		private static void CreateInstance()
+		{
+			var singleton = new GameObject();
+			_instance = singleton.AddComponent<T>();
+			singleton.name = $"{typeof(T)}(singleton)";
+
+			var component = _instance.GetComponent<Singleton<T>>();
+			component.OnCreate();
+
+			if (component.ShouldNotDestroyOnLoad()) DontDestroyOnLoad(singleton);
+			Log.Show(MSG_CREATE_INSTANCE, DEBUG_TYPE, typeof(T));
+		}
+
+		private void OnDestroy () 
+		{
+            _instance = null;
+		}
+	}
+}
