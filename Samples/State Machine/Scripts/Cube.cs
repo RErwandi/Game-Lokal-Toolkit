b605022c5a0869139d3d77c7a60d9158a6e1e6b8
Reynold Erwandi <reynolderwandi@gmail.com>
2020-05-13 04:00:33 +0700

    Add State Machine

---
+++ Samples/State Machine/Scripts/Cube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Material _material;
+    
+    void Start()
+    {
+        _material = GetComponent<Renderer>().material;
+    }
+
+    public void TurnRed()
+    {
+        _material.color = Color.red;
+    }
+    
+    public void TurnGreen()
+    {
+        _material.color = Color.green;
+    }
+    
+    public void TurnBlue()
+    {
+        _material.color = Color.blue;
+    }
+}
