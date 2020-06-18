cbe3a3bb2fb5e9207da0a669be763c74add78b90
Reynold Erwandi <reynolderwandi@gmail.com>
2020-06-18 13:25:33 +0700

    Add Better Events and change package vesion to 2.0.0

---
+++ Assets/com.gamelokal.toolkit/Editor/Better Events/BetterEventListDrawer.cs
@@ -0,0 +1,15 @@
+﻿using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using UnityEngine;
+
+namespace GameLokal.Utility
+{
+    public class BetterEventListDrawer : OdinValueDrawer<BetterEvent>
+    {
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            this.Property.Children["Events"].Draw(label);
+        }
+    }
+}
---
+++ Assets/com.gamelokal.toolkit/Editor/Better Events/DelegateInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace GameLokal.Utility
+{
+    public struct DelegateInfo
+    {
+        public UnityEngine.Object Target;
+        public MethodInfo Method;
+    }
+}
