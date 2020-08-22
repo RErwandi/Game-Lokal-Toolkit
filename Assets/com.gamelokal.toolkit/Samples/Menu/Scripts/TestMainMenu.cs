870ae5e2a57efdfcdd3bfc0b01e3c7b8c359020f
Reynold Erwandi <reynolderwandi@gmail.com>
2020-08-22 13:30:31 +0700

    Make start menu optional and samples menu

---
+++ Assets/com.gamelokal.toolkit/Samples/Menu/Scripts/TestMainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameLokal.Utility.MenuManager;
+using UnityEngine;
+
+public class TestMainMenu : Menu<TestMainMenu>
+{
+    public void OpenPanel()
+    {
+        TestPanelMenu.Open();
+    }
+
+    public void OpenPopup()
+    {
+        TestPopupMenu.Open();
+    }
+}
---
+++ Assets/com.gamelokal.toolkit/Samples/Menu/Scripts/TestPanelMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameLokal.Utility.MenuManager;
+using UnityEngine;
+
+public class TestPanelMenu : Menu<TestPanelMenu>
+{
+    public override void OnBackPressed()
+    {
+        base.OnBackPressed();
+        TestMainMenu.Open();
+    }
+}
